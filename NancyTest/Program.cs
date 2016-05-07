44af0c984fc988150f83993c4a776ec6cc95803d
Arno den Uijl <aduijl@sogyo.nl>
2016-05-07 20:24:57 +0200

    hello world to test docker windows 2016

---
+++ NancyTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace NancyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = new Uri("http://localhost:1234");
+            var config = new HostConfiguration {RewriteLocalhost = false};
+
+            using (var host = new NancyHost(config, uri))
+            {
+                host.Start();
+                Console.ReadLine();
+                host.Stop();
+            }
+        }
+    }
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule() : base("/")
+        {
+            Get["/"] = o => "Hello world!";
+        }
+    }    
+}
